dcdaa514eef014418e506b2f0d82c65a493597d4
zz不知道 <zz821199495@163.com>
2017-03-21 21:33:03 +0800

    3-21 修改前保存一下代码

--- app/src/main/java/com/kiritoweather/android/WeatherActivity.java
+++ app/src/main/java/com/kiritoweather/android/WeatherActivity.java
@@ -78,6 +78,7 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 设置系统状态栏背景
         if (Build.VERSION.SDK_INT >= 21){
             View decorView = getWindow().getDecorView();
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
